c8d65f075e5abfcf33f69a2baee130922181be6e
lucianasomariva09 <159184331+lucianasomariva09@users.noreply.github.com>
2024-10-01 16:40:43 -0300

    01/10

---
+++ pergunta.js
@@ -0,0 +1,15 @@
+criaCartao(
+    'Programaçao',
+    'O que Python',
+    'O Pyton é uma linguagem de programaçao'
+)
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França',
+    'a capital da França é Paris'
+)
+criaCartao(
+    'Programaçao',
+    'O que é funçao',
+    'Uma funçao é um bloco de código que executa alguma tarefa'
+)
